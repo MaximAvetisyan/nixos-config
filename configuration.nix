0a8413ddc6bee5947319cd9bc6cec608c63dca2b
MaximAvetisyan <maxwwhi@gmail.com>
2025-12-28 01:09:03 +0300

    feat: Moved i3 -> Wayland, nvidia drivers

--- configuration.nix
+++ configuration.nix
@@ -2,17 +2,65 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, callPackage, ... }:
-
+{ config, pkgs, ... }:
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz";
+in
 {
+ # Enable OpenGL
+  hardware.graphics = {
+    enable = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    open = false;
+
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+
   imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+    [ 
+    (import "${home-manager}/nixos")
+    ./hardware-configuration.nix
     ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -44,56 +92,133 @@
 
   # Configure keymap in X11
   services.xserver.xkb = {
-    layout = "us,ru";
+    layout = "us";
     variant = "";
-    options = "grp:alt_shift_toggle";
   };
 
+  # Display manager
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet \
+          --time --time-format '%I:%M %p | %a • %h | %F' \
+          --cmd 'uwsm start hyprland'";
+        user    = "greeter";
+      };
+    };
+  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.maxim = {
+  users.users.max = {
     isNormalUser = true;
-    description = "maxim";
-    extraGroups = [ "networkmanager" "wheel" ];
+    description = "max";
+    extraGroups = [ "networkmanager" "wheel" "sudo" "input" ];
     packages = with pkgs; [];
   };
-  
-  environment.pathsToLink = ["/libexec"];
-  services.xserver = {
-  	enable = true;
-	desktopManager = {
-		xterm.enable = false;
-	};
-	displayManager = {
-		defaultSession = "none+i3";
-	};
-	windowManager.i3 = {
-		enable = true;
-		extraPackages = with pkgs; [
-			dmenu
-			i3status
-			i3lock
-			i3blocks
-		];
-	};
-  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
+  programs.hyprland = {
+	enable = true;
+	withUWSM = true;
+	xwayland.enable = true;
+  };
+
+  programs.waybar.enable = true;
+
+  home-manager.users.max = {
+    home.stateVersion = "25.11";
+    programs.waybar = {
+      enable = true;
+      systemd.enable = true; # Automatically starts waybar with your session
+      
+      # Layout and Module Settings
+      settings = [{
+        layer = "top";
+        position = "top";
+        height = 30;
+        modules-left = [ "hyprland/workspaces" ];
+        modules-center = [ "clock" ];
+        modules-right = [ "tray" ];
+
+        # Module-specific configurations
+        "hyprland/workspaces" = {
+        	format = "{name}"; # Shows only the number/name
+        	on-click = "activate";
+        	sort-by-number = true;
+        	all-outputs = true;
+        	active-only = false;
+        };
+
+        clock = {
+          format = "{:%H:%M}  ";
+          tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+        };
+
+      }];
+
+      # CSS Styling
+      style = ''
+        * {
+          border: none;
+          font-family: "JetBrainsMono Nerd Font";
+          font-size: 14px;
+        }
+        window#waybar {
+          background-color: rgba(26, 27, 38, 0.9);
+          border-bottom: 2px solid #7aa2f7;
+          color: #c0caf5;
+        }
+        #workspaces button {
+          padding: 0 5px;
+          color: #7aa2f7;
+        }
+        #workspaces button.active {
+          color: #bb9af7;
+        }
+        #clock {
+          padding: 0 10px;
+        }
+      '';
+    };
+    home.pointerCursor = {
+      gtk.enable = true;
+      x11.enable = true;
+      package = pkgs.apple-cursor;
+      name = "macOS";
+      size = 24;
+    };
+
+  };
+  
+
   environment.systemPackages = with pkgs; [
- 	git
+
+	neovim
+	wget
+	git
+	curl
+	chromium
+	pkgs.ghostty
+	pkgs.kitty
+	pkgs.apple-cursor
 	go
-  	unzip
- 	zig
- 	clang
- 	neovim
- 	pkgs.ghostty
- 	curl
- 	wget
- 	tmux
- 	xclip
- 	firefox
+	unzip
+	zig
+	clang
+	tmux
+	greetd.tuigreet
+	cmake
+	wofi
+	waybar
+	hyprpaper
+	gcc
   ];
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
   fonts.packages = with pkgs; [
   	meslo-lgs-nf
@@ -104,7 +229,7 @@
       enable = true;
       enableCompletion = true;
       autosuggestions.enable = false;
-      syntaxHighlighting.enable = true;
+      syntaxHighlighting.enable = false;
       ohMyZsh = {
         enable = true;
         theme = "robbyrussell";
@@ -121,8 +246,22 @@
 	  vim = "nvim";
         };
     };
+    tmux = {
+      enable = true;
+      clock24 = true;
+      plugins = with pkgs.tmuxPlugins; [
+        sensible
+        resurrect
+        vim-tmux-navigator
+      ];
+      extraConfig = ''
+        set -g mouse on
+        set -g base-index 1
+        setw -g pane-base-index 1
+        set -g status-style bg=default
+      '';
+    };
   };
-
   users.defaultUserShell = pkgs.zsh;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -135,13 +274,13 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  # services.openssh.enable = true;
 
   # Open ports in the firewall.
-  #networking.firewall.allowedTCPPorts = [ ... ];
-  #networking.firewall.allowedUDPPorts = [ ... ];
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -149,6 +288,5 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
-
+  system.stateVersion = "25.11"; # Did you read the comment?
 }
